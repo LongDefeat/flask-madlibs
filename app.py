733b78cd3e4e3573730a9ba6e6af1ca8de5aa489
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2021-11-14 16:55:57 -0800

    springboard madlibs exercise with flask
    html files are templates

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def ask_story():
+    """Show list-of-stories form."""
+
+    return render_template("select-story.html",
+                           stories=stories.values())
+
+@app.route("/questions")
+def ask_questions():
+    """Generate and show form to ask words."""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+    prompts = story.prompts
+    return render_template("questions.html", story_id=story_id, title=story.title, prompts=prompts)
+
+@app.route("/story")
+def show_story():
+    """show the story"""
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+    text = story.generate(request.args)
+    return render_template("story.html", title=story.title,text=text)
+
+
